e3b0d461abfec2143a4653e713525fccc8cf2772
Nomad9-0 <ojimenezlmpc@gmail.com>
2019-02-20 17:45:38 -0800

    functioning game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 //list of words
-var monsters = ['beholder', 'owlbear', 'dragon', 'goblin', 'vampire', 'ogre'];
+var monsters = ['beholder', 'owlbear', 'dragon', 'goblin', 'vampire', 'ogre', 'ork', 'wearwolf'];
+var monsterImages = [];
 
 //global scope
 var wordJoin;
@@ -11,6 +12,13 @@ var wins = 0;
 var guesses = 10;
 var guess;
 var wrongLetter = [];
+var wrongGuess = true;
+
+//win count
+document.getElementById("winCount").innerHTML = 'Wins: ' + wins;
+
+//loss count
+document.getElementById("lossCount").innerHTML = guesses;
 
 //pick random word
 var word = monsters[Math.floor(Math.random() * monsters.length)];
@@ -18,57 +26,59 @@ var word = monsters[Math.floor(Math.random() * monsters.length)];
 //blank answer
 var answer = [];
 
+//////////////////  Game  ///////////////////
 //loops through random word and converts letters to _, then joins
 function startUp(){
     for (var i = 0; i < word.length; i++) {
         answer[i] = '_';
         wordJoin = answer.join(' ');
     }
-    document.getElementById("currentWord").innerHTML = wordJoin;
-    
+    document.getElementById("currentWord").innerHTML = wordJoin;  
 }
-//////////////////  Game  ///////////////////
-
 //compares key pressed to letter in word
-window.addEventListener('keydown', function(evt){
+window.addEventListener('keyup', function (evt){
     guess = String.fromCharCode(evt.keyCode).toLowerCase();
-    for (var j = 0; j < word.length; j++) {
-        if (guess === word[j]) {
-            answer[j] = guess.toUpperCase();
-            document.getElementById("currentWord").innerHTML =  answer;
-        } else { //add wrong guess to wrongLetter array
-            wrongLetter.push(guess.toUpperCase());
-            //kill duplicates here somehow
-            document.getElementById("guessedLetter").innerHTML =  wrongLetter;
-        } 
+    //loop through word and check if guess is in word
+    if (word.includes(guess)) { //true
+        for (var j = 0; j < word.length; j++) {
+            if (guess === word[j]) {
+                answer[j] = guess.toUpperCase();
+                document.getElementById("currentWord").innerHTML =  answer;
+                //if word guessed correctly, alert win           
+                if (!answer.includes('_')) {
+                    //add img
+
+                    setTimeout(function() {
+                        if (window.confirm("You Win! Play Again?")) {
+                            //remove last word here somehow
+                            word = monsters[Math.floor(Math.random() * monsters.length)];
+                            startUp();
+                            wins++;
+                            document.getElementById("winCount").innerHTML = 'Wins: ' + wins;
+                        }
+                    }, 10)
+                }   
+            } 
+        }
+    // if guess is not in word add to wrongLetter array and sub from lives
+    } else {
+        wrongLetter.push(guess.toUpperCase());
+        document.getElementById("guessedLetter").innerHTML =  wrongLetter;
+        //reduce lives
+        guesses--;
+        document.getElementById("lossCount").innerHTML =  guesses;
+        if(guesses == 0) {
+            if (window.confirm("You Lose! Play Again?")) {
+                word = monsters[Math.floor(Math.random() * monsters.length)];
+                startUp();
+                guesses = 10;
+                document.getElementById("lossCount").innerHTML = guesses;
+            }
+        }
     }
 })
 
-
-
-    //document.getElementById("guessedLetter").innerHTML = guess;
-    // if(guess === answer[i]){
-    //     console.log("works")
-    // }    
-
-
-// function letterGuessed() {
-//     var guess = document.getElementById('letter').value;
-// }
-// if (guess.length > 0) {
-//     // check to see if key matches letter on word
-//     // if match, replace _
-
-//     for (var i = 0; i < word.length; i++) {
-//         //letter is in word
-//         if (word[j] === guess) {
-//             answer[j] = guess;
-//         }
-//     }
-// }
 /////////////////  Game End  /////////////////
-
-
 
 
 startUp();
